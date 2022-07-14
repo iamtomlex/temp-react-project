65e4f1852e7f3fb7df55b795deac091fe120dbbc
iamtomlex <101986771+iamtomlex@users.noreply.github.com>
2022-07-14 10:57:38 -0700

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias hic,
+        dignissimos saepe consequatur, dolorem asperiores ad, nulla numquam
+        reprehenderit provident nesciunt. Nesciunt beatae esse, magni possimus
+        unde molestiae iure quae inventore sapiente aut! At maxime corporis quia
+        corrupti, facilis in!
+      </p>
+    </section>
+  )
+}
+
+export default About
